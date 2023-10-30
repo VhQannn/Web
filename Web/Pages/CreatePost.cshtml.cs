aa48422347ce7e57b263e065448f5a25d2e88624
Quanvo313 <vhquan313@gmail.com>
2023-10-30 17:18:25 +0700

    fe sida

---
+++ Web/Pages/CreatePost.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Web.DbConnection;
+
+namespace Web.Pages
+{
+    public class CreatePostModel : PageModel
+    {
+        private readonly Web.DbConnection.WebContext _context;
+
+        public CreatePostModel(Web.DbConnection.WebContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return Page();
+        }
+
+        [BindProperty]
+        public Post Post { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Posts == null || Post == null)
+            {
+                return Page();
+            }
+
+            _context.Posts.Add(Post);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
