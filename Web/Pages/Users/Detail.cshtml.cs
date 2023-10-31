01790444c2d5876e499a25693d5f5576fd232d25
Quanvo313 <vhquan313@gmail.com>
2023-10-31 17:59:44 +0700

    add user detail

---
+++ Web/Pages/Users/Detail.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Web.DbConnection;
+
+namespace Web.Pages.Users
+{
+    public class DetailModel : PageModel
+    {
+        public User User { get; set; } = default!;
+
+        public DetailModel()
+        {
+        }
+
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+
+            return Page();
+        }
+    }
+}
