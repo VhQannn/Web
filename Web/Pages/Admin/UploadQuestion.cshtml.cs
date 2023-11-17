cf7efed485ab842acfd369271d7d476e5c8d1aca
VhQannn <qanvo313@gmail.com>
2023-11-17 23:33:00 +0700

    pikapiii

--- Web/Pages/Admin/UploadQuestion.cshtml.cs
+++ Web/Pages/Admin/UploadQuestion.cshtml.cs
@@ -1,12 +1,10 @@
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Web.Pages.Admin
 {
-    [Authorize(Roles = "Admin")]
-    public class PostManagementModel : PageModel
+    public class UploadQuestionModel : PageModel
     {
-
         public void OnGet()
         {
         }
