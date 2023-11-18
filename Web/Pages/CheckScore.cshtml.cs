a91b219e68900bcfb2768c94c1e94c7769e4e778
tuankietlp <tuankietlp2@gmail.com>
2023-11-18 19:16:23 +0700

    Bat loi chua co examcode

--- Web/Pages/CheckScore.cshtml.cs
+++ Web/Pages/CheckScore.cshtml.cs
@@ -1,7 +1,8 @@
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Security.Claims;
+using Web.Controllers;
 using Web.DbConnection;
 using Web.DTOs;
 
@@ -14,11 +15,12 @@ namespace Web.Pages
         public int UserId { get; set; }
         private readonly WebContext _context;
         public List<string> CurrentCourses { get; set; } = new List<string>();
-        public CheckScoreModel(IHttpClientFactory clientFactory, WebContext context)
+        private readonly MarkReportServices _markReportServices;
+        public CheckScoreModel(IHttpClientFactory clientFactory, WebContext context, MarkReportServices markReportServices)
         {
             _clientFactory = clientFactory;
             _context = context;
-
+            _markReportServices = markReportServices;
         }
         public void OnGet()
         {
@@ -31,26 +33,37 @@ namespace Web.Pages
         {
             if (file != null && file.Length > 0)
             {
-                var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
-                UserId = userIdClaim != null ? int.Parse(userIdClaim.Value) : 1;
-                var client = _clientFactory.CreateClient();
-                var absoluteUri = "https://hotrohoctap.azurewebsites.net/api/upload";
-                using var content = new MultipartFormDataContent();
-                using var fileStream = file.OpenReadStream();
-                using var streamContent = new StreamContent(fileStream);
-                content.Add(streamContent, "file", file.FileName);
-                content.Add(new StringContent(UserId.ToString()), "userId");
-                var response = await client.PostAsync(absoluteUri, content);
-
-                if (response.IsSuccessStatusCode)
+                string examCode = await _markReportServices.GetTemplateCodeFromFile(file);
+                var checkExistExamcode = _context.QuestionTemplates.FirstOrDefault(x => x.QuestionTemplateCode == examCode);
+                if(checkExistExamcode != null)
                 {
-                    var responseUri = await response.Content.ReadFromJsonAsync<MarkReport1DTO>();
-                    return RedirectToPage("/MyPayment");
+                    var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+                    UserId = userIdClaim != null ? int.Parse(userIdClaim.Value) : 1;
+                    var client = _clientFactory.CreateClient();
+                    var absoluteUri = "https://hotrohoctap.azurewebsites.net/api/upload";
+                    using var content = new MultipartFormDataContent();
+                    using var fileStream = file.OpenReadStream();
+                    using var streamContent = new StreamContent(fileStream);
+                    content.Add(streamContent, "file", file.FileName);
+                    content.Add(new StringContent(UserId.ToString()), "userId");
+                    var response = await client.PostAsync(absoluteUri, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseUri = await response.Content.ReadFromJsonAsync<MarkReport1DTO>();
+                        return RedirectToPage("/MyPayment");
+                    }
+                    else
+                    {
+                        TempData["AlertMessage"] = "Mã đề này chưa hỗ trợ";
+                        return Page();
+                    }
                 }
                 else
                 {
+                    TempData["AlertMessage"] = "Mã đề này chưa hỗ trợ";
                     return Page();
-                }
+                }   
             }
             return Page();
         }
